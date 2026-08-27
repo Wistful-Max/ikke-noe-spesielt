92536bba77250a544bbe091a822f4e4550b3418d
Wistful-max <matia013@osloskolen.no>
2026-08-27 14:09:05 +0200

    Counters work
    The buttons and point counters go back and forth now.

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ add.addEventListener('click',function(){
     amount = amount + 1;
     document.getElementById("counter").textContent= amount
 })
+
+let amount2 = 0
+const add2 = document.getElementById("plus2")
+add2.addEventListener('click',function(){
+    amount2 = amount2 + 1;
+    document.getElementById("counter2").textContent= amount2
+})
+
+/*remove*/
+const remove = document.getElementById("minus")
+remove.addEventListener('click',function(){
+    amount = amount - 1;
+    document.getElementById("counter").textContent= amount
+})
+
+const remove2 = document.getElementById("minus2")
+remove2.addEventListener('click',function(){
+    amount2 = amount2 - 1;
+    document.getElementById("counter2").textContent= amount2
+})
